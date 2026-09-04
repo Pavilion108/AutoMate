e377054fc6f4a7dfc29c703fd32f35496fd811ef
Pavilion108 <home.pavilion1975@gmail.com>
2026-09-04 14:24:57 +0530

    Remove unused WorkManager + startup-runtime causing InitializationProvider crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,14 +83,6 @@ dependencies {
     // Location / Geofencing
     implementation("com.google.android.gms:play-services-location:21.3.0")
 
-    // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.10.0")
-    implementation("androidx.hilt:hilt-work:1.2.0")
-    ksp("androidx.hilt:hilt-compiler:1.2.0")
-
-    // Startup (required by WorkManager for InitializationProvider)
-    implementation("androidx.startup:startup-runtime:1.1.1")
-
     // JSON
     implementation("com.google.code.gson:gson:2.11.0")
 
